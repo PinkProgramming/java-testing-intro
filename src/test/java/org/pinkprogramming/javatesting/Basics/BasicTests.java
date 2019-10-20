8f8a377ffabf881dae1580453357347a38e4e7c3
Taiza Montenegro <taiza.montenegro@izettle.com>
2019-10-20 09:21:02 +0200

    Add solution for test that call a method

--- src/test/java/org/pinkprogramming/javatesting/Basics/BasicTests.java
+++ src/test/java/org/pinkprogramming/javatesting/Basics/BasicTests.java
@@ -4,9 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.pinkprogramming.javatesting.util.SentenceUtil;
 
 public class BasicTests {
 
+    // Test 1. Numerical comparisons
     @Test
     @DisplayName("The vehicle should be affordable")
     void vehicleShouldBeAffordable() {
@@ -17,12 +19,30 @@ public class BasicTests {
         assertTrue((fuel + hullRepairs) < maxCost);
     }
 
+    // Test 2. String comparisons
     @Test
     @DisplayName("Should end with a period.")
     void sentenceEndsWithAPeriod() {
         String example = "Hi, my name is Polly.";
 
         assertTrue(example.endsWith("."));
+    }
+
+    // Test 2 b. Revisit test2
+    // this time testing a method
+    // that accepts a string and returns last character
+    @Test
+    @DisplayName("Method should return true when sentence ends with period")
+    void shouldReturnTrueWhenSentenceEndsWithPeriod() {
+        String example = "Hi, my name is Polly.";
+        assertTrue(SentenceUtil.isEndWithPeriodString(example));
+    }
+
+    @Test
+    @DisplayName("Method should return false when sentence does not end with period")
+    void shouldReturnFalseWhenSentenceDoesNotEndWithPeriod() {
+        String example = "Hi";
+        assertTrue(SentenceUtil.isEndWithPeriodString(example));
     }
 
 }
